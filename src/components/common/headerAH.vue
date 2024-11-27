314b331c2a4f0ac4a7bb57ed7b17e6b3a5b9441c
JumMy777 <jummy1806@gmail.com>
2024-11-27 18:14:43 +0800

    ++added HeaderVue , -- changed userype in registratiom,  --and chaned route in home

---
+++ src/components/common/headerAH.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-toolbar color="light-green-lighten-2">
+    <v-toolbar-title>
+      <router-link
+      class="text-decoration-none text-black"
+      :to="{ path: '/' }"> AgriHUB</router-link>
+    </v-toolbar-title>
+  </v-toolbar>
+</template>
